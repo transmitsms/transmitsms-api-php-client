7310483e0204ce64db1304caaf70affc05e6cf41
neil <neil@burstsms.com>
2020-06-24 13:11:53 +1000

    added get-number

---
+++ examples/Numbers/get-number.php
@@ -0,0 +1,12 @@
+<?php
+include 'APIClient2.php';
+
+$api = new transmitsmsAPI('API_KEY', 'API_SECRET');
+$result = $api->getNumber('6140000000');
+
+if ($result->error->code == 'SUCCESS') {
+    echo 'This number is {$result->status} and valid up to {$result->next_charge}';
+
+} else {
+    echo 'Error: {$result->error->description}';
+}
